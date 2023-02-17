1c35ae804fa80b0ce382a24efdefd02296f25745
dongqishu <dongqishu@126.com>
2023-02-17 11:44:18 +0800

    Update Ble.js

--- www/Ble.js
+++ www/Ble.js
@@ -3,3 +3,10 @@ var exec = require('cordova/exec');
 exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'Ble', 'coolMethod', [arg0]);
 };
+
+exports.startAdvertising = function (uuid, major, minor, txPower, success, error) {
+    exec(success, error, 'Ble', 'startAdvertising', [uuid, major, minor, txPower]);
+};
+exports.stopAdvertising = function (success, error) {
+    exec(success, error, 'Ble', 'stopAdvertising', []);
+};
